409e1c4189dee84fbc39b8b915b748ff5fbd1482
MalwarePerseus <malwareperseus@gmail.com>
2020-06-05 15:38:10 +0530

    Added Right-Click Context Menu with Quit option

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.on('ready', ()=> {
 
 	const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
 	const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-	new TimerTray(iconPath, mainWindow);
+	tray = new TimerTray(iconPath, mainWindow);
 });
 
