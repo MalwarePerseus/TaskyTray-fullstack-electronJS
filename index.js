f15ddd655a655713e8165df2f553ce7c002f4562
MalwarePerseus <malwareperseus@gmail.com>
2020-06-05 16:01:50 +0530

    Icon is now hidden in the Dock and Taskbar.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let mainWindow;
 let tray;
 
 app.on('ready', ()=> {
+	if(process.platform === 'darwin') {app.dock.hide();};
 	mainWindow = new BrowserWindow({
 		webPreferences: {
 			nodeIntegration: true,
@@ -16,12 +17,13 @@ app.on('ready', ()=> {
 		resizable: false,
 		frame: false,
 		show: false,
+		skipTaskbar:true
 	});
 	mainWindow.loadFile('./src/index.html');
 	mainWindow.on('blur', ()=>{
 		mainWindow.hide();
 	});
-	
+
 	const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
 	const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 	tray = new TimerTray(iconPath, mainWindow);
